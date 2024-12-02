cad109b336d978356ceef44459594a28b3aabe5d
user <user@WINDOWS-5AN5BRH>
2024-12-02 20:14:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Predicate<int> isPositive = num => num > 0;
+
+        Console.WriteLine("Введіть число:");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (isPositive(num))
+        {
+            Console.WriteLine($"{num} є додатним числом.");
+        }
+        else
+        {
+            Console.WriteLine($"{num} не є додатним числом.");
+        }
+    }
+}
